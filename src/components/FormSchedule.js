42f01e8fa1ba7df4855735e0d95d249d56e2ab9c
Maria <mit.maria2019@yandex.ru>
2023-09-11 23:34:57 +0300

    feat: аллерт с предупреждением и обработка формы

--- src/components/FormSchedule.js
+++ src/components/FormSchedule.js
@@ -1,36 +1,48 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button, Alert } from "react-native";
 
 export const FormSchedule = (props) => {
+    const [value, setValue] = useState([]);
+
     const pressHandler = () => {
-        props.onSubmit('Пример 1')
-    }
+        if(value.trim()) {
+            props.onSubmit(value)
+            setValue('')
+        } else {
+            Alert.alert('Название предмета не может быть пустым!')
+        }
+    };
 
     return (
         <View style={styles.container}>
-            <TextInput style={styles.input} />
-            <Button onPress={pressHandler} title='Кнопка' />
+            <TextInput
+                style={styles.input}
+                onChangeText={setValue}
+                value={value}
+                placeholder="Введите название предмета"
+            />
+            <Button onPress={pressHandler} title="Кнопка" />
         </View>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#8312a8",
         alignItems: "center",
         justifyContent: "center",
-        flexDirection: 'row',
-        padding: 10, 
-        gap: 8
+        flexDirection: "row",
+        padding: 10,
+        gap: 8,
     },
     text: {
         color: "#fff",
-        fontSize: 32
+        fontSize: 32,
     },
     input: {
-        width: '80%',
-        borderStyle: 'solid',
+        width: "80%",
+        borderStyle: "solid",
         padding: 10,
-        backgroundColor: '#fff'
-    }
+        backgroundColor: "#fff",
+    },
 });
